3a7a6baa91398f5347ef55ab76b5798c4968a0f1
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-12-18 14:38:29 +0000

    results get sent to database

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from werkzeug import exceptions
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 CORS(app)
+
+db = SQLAlchemy(app)
+
+class Results(db.Model):
+    _id = db.Column('id', db.Integer, primary_key=True)
+    calculation = db.Column('calculation', db.Float)
+
+    def __init__(self, calculation):
+        self.calculation = calculation
 
 @app.route('/')
 def home():
@@ -36,7 +48,14 @@ def result():
             case 'athlete':
                 calculation = int(bmr * 1.9)
         print(bmr)
+        calc = Results(calculation)
+        db.session.add(calc)
+        db.session.commit()
     return render_template('base.html', form_data = form_data), 200
+
+@app.route('/view')
+def view():
+    return render_template('view.html', values = Results.query.all())
 
 @app.errorhandler(exceptions.NotFound)
 def handle_404(err):
@@ -47,5 +66,7 @@ def handle_500(err):
     return jsonify({"message": f"It's not you, it's us"}), 500
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run()
 
